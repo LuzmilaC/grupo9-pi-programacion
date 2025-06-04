b2dccbb1abdbd0e0cd8e24c98aaaa79ebf3b5c9c
matiasblousson <mblousson@udesa.edu.ar>
2025-06-04 17:45:03 -0300

    Cambie url y estilo

--- js/moviegenres.js
+++ js/moviegenres.js
@@ -1,28 +1,6 @@
-const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=2e7092285b99d972d514083dff1b0746";
+const url = "https://api.themoviedb.org/3/genre/movie/list?api_key=2e7092285b99d972d514083dff1b0746";
 let resultados = document.querySelector(".generos");
 let rtas = ""
-const generos = {
-  28: "Acción",
-  12: "Aventura",
-  16: "Animación",
-  35: "Comedia",
-  80: "Crimen",
-  99: "Documental",
-  18: "Drama",
-  10751: "Familia",
-  14: "Fantasía",
-  36: "Historia",
-  27: "Terror",
-  10402: "Música",
-  9648: "Misterio",
-  10749: "Romance",
-  878: "Ciencia ficción",
-  10770: "TV",
-  53: "Suspenso",
-  10752: "Bélica",
-  37: "Western"
-};
-let generosAgregados = [];
 
 fetch(url)
   .then(function(response) {
@@ -30,26 +8,12 @@ fetch(url)
   })
   .then(function(data) {
     console.log(data);
-    let result = data.results;
+    let result = data.genres;
     for (let i = 0; i < result.length; i++) {
-      let todos = result[i].genre_ids;
-      for (let j = 0; j < todos.length; j++) {
-        let id = todos[j];
-        let genero = generos[id];
-        let yaAgregado = false;
-
-        for (let k = 0; k < generosAgregados.length; k++) {
-          if (generosAgregados[k] == genero) {
-            yaAgregado = true;
-            break;
-          }
-        }
-
-        if ((yaAgregado == false) ) {
-          generosAgregados.push(genero);
-          rtas += `<h2><a href="detailMovie-genres.html">${genero}</a></h2>`;
-        }
-      }
+        let genero = result[i].name;
+        let id = result[i].id;
+        rtas += `
+        <h2><a href="detailMovie-genres.html?id=${id}">${genero}</a></h2>`;
     }
     resultados.innerHTML = rtas;
   })
@@ -57,6 +21,19 @@ fetch(url)
     console.log("Error: " + error);
     document.querySelector(".cargando").style.display = "none";
   });
+window.addEventListener("load", function () {
+    let botones = document.querySelectorAll(".textTitulo");
+
+    for (let i = 0; i < botones.length; i++) {
+        botones[i].addEventListener("mouseover", function () {
+            this.style.color = "rgb(100, 200, 255)";
+        });
+
+        botones[i].addEventListener("mouseout", function () {
+            this.style.color = ""; // vuelve al color original
+        });
+    }
+});
 window.addEventListener("load", function () {
     let botones = document.querySelectorAll(".textTitulo");
 
