9ad61b0cbd6259576b543937edb5d0877de1f9d1
matiasblousson <mblousson@udesa.edu.ar>
2025-06-06 23:53:38 -0300

    Sume el window que no estaba

--- js/detail-serie-genre.js
+++ js/detail-serie-genre.js
@@ -50,3 +50,16 @@ fetch(urlSeries)
     console.error("Error al cargar los datos:", error);
     tituloGenero.innerText = "Ocurrió un error al cargar la página.";
   });
+  window.addEventListener("load", function () {
+    let botones = document.querySelectorAll(".textTitulo");
+
+    for (let i = 0; i < botones.length; i++) {
+        botones[i].addEventListener("mouseover", function () {
+            this.style.color = "rgb(100, 200, 255)";
+        });
+
+        botones[i].addEventListener("mouseout", function () {
+            this.style.color = ""; 
+        });
+    }
+});
